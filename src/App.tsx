934a7f8d5d416e18d75be77c1f7f9936151b0f6f
jshprintz <jshprintz@gmail.com>
2022-12-29 15:44:52 -0800

    added some styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import TodoStore from "./stores/TodoStore";
 const todos = new TodoStore();
 
 const App = () => {
-  return <div className="App">
+  return <div>
     <TodoInput todos={todos}/>
     <TodoList todos={todos}/>
   </div>;
--- src/Todo/TodoInput/index.tsx
+++ src/Todo/TodoInput/index.tsx
@@ -16,7 +16,7 @@ const TodoInput = ({ todos }: { todos: TodoStore }) => {
 
   return (
     <InputWrapper>
-      <input value={newTodo} onChange={handleInputChange} />
+      <Input value={newTodo} onChange={handleInputChange} />
       <Button onClick={handleButtonClick}>ADD</Button>
     </InputWrapper>
   );
@@ -31,11 +31,32 @@ const InputWrapper = styled.div`
   margin-top: 10vh;
 `;
 
+const Input = styled.input `
+  border: 2px solid limegreen;
+  box-shadow: 0 0 10px grey;
+  border-radius: 5px;
+
+  margin: 5px;
+`
+
 const Button = styled.button`
   height: 5vh;
   width: 8vw;
 
   background-color: pink;
+  margin: 5px;
+  cursor: pointer;
+
+  &:hover{
+    background-color: limegreen;
+    animation: shadow .5s linear infinite;
+    box-shadow: 0 0 10px grey;
+
+    @keyframes shadow {
+      0% {box-shadow: 0 0 0px green}
+      100% {box-shadow: 0 0 50px green}
+    }
+  }
 `;
 
 export default TodoInput;
